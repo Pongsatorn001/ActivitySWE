590d130bc02b7d6772f9d6b069a334262f5a577f
DESKTOP-QQ4UMN4\asus <Pongsatorn698@gmail.com>
2017-11-27 03:01:22 +0700

    Update two

--- app/config/config.php
+++ app/config/config.php
@@ -12,7 +12,7 @@ return new \Phalcon\Config([
         'host'        => 'localhost',
         'username'    => 'root',
         'password'    => '',
-        'dbname'      => 'test',
+        'dbname'      => 'activity (1)',
         'charset'     => 'utf8',
     ],
     'application' => [
